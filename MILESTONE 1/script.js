4f7f4747faaa56a778842bf0e3796ee27dd8f054
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-12-09 17:02:10 +0100

    inizio

---
+++ MILESTONE 1/script.js
@@ -0,0 +1,14 @@
+Vue.config.devtools = true;
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            todos: [
+                'Fare la colazione',
+                'Portare fuori il cane',
+                'Preparare il pranzo',
+                'Fare l\'allenamento',
+            ]
+        }
+    }
+)
